cd11b8c62bd3ab86f39a794a73f18a3e09aa81a1
usuario <usuario@DESKTOP-DM94O17>
2024-06-09 12:13:38 -0300

    Commit 1-Base

---
+++ TP_04-EmilioMamani/src/main/java/ar/adu/unju/fi/Tp04EmilioMamaniApplication.java
@@ -0,0 +1,13 @@
+package ar.adu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp04EmilioMamaniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp04EmilioMamaniApplication.class, args);
+	}
+
+}
